b38462e4ff7fd36f0e5557f980066da0b0273ef6
Samuel Banya <sbanya@fastmail.com>
2022-07-10 18:38:13 -0500

    commit tree 556db58cd86d9a0046b42acc49c6e0ec54227904

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
+import React, { useState, useEffect } from "react";
+
 // create your App component here
+function App() {
+  const [dogImage, setDogImage] = useState(false);
+
+  let imgLocation = "";
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        imgLocation = data["message"];
+        console.log("imgLocation: ", imgLocation);
+        setDogImage(imgLocation);
+      });
+  }, []);
+  // <img src="https://images.dog.ceo/breeds/terrier-welsh/lucy.jpg" />
+
+  if (!dogImage) return <h3>Loading...</h3>;
+
+  return (
+    <div>
+      <img alt="A Random Dog" src={dogImage} />
+    </div>
+  );
+}
+
+export default App;
